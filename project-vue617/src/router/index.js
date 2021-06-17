08c6f1ed6039ec7fd29b44df6864f9a2dcd927ae
yiyun Tseng <>
2021-06-17 21:40:02 +0800

    (34行紅色底線問題)

--- project-vue617/src/router/index.js
+++ project-vue617/src/router/index.js
@@ -15,11 +15,16 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/newpage',
+    name: '新增tset頁面',
+    component: () => import('../views/NewPage.vue'),
+  },
 ];
 
-const router = createRouter({
+const routertest = createRouter({
   history: createWebHashHistory(),
   routes,
 });
 
-export default router;
+export default routertest;
